81d3fb962fde775e2eac383b3d54f3b01d29d59e
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-05-23 17:08:45 +0530

    Commit-3:Version 2

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ var btn = document.getElementById("add");
 btn.addEventListener("click", e => {
   addNote();
 });
-
-var notes = [];
 var c = 1;
-var pagearea = document.querySelector(".col-9");
+var notes = [];
+bringNotes();
+postNotes();
+
+//Function To Add Notes
+
 function addNote() {
   var form = document.getElementById("note-form");
   var title = document.getElementById("title").value;
@@ -25,12 +28,35 @@ function addNote() {
   }
   document.getElementById("title").value = "";
   document.getElementById("contentbody").value = "";
+  saveNotes();
   postNotes();
   c++;
   btn.innerHTML = "Add";
 }
 
+// Function to Save Notes to Local Storage
+
+function saveNotes() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+//Function to Bring Notes from Local Storage
+
+function bringNotes() {
+  if (localStorage.getItem("notes") != null) {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  } else {
+    notes = [];
+  }
+  c = notes.length + 1;
+  console.log(c);
+  console.log(notes);
+}
+
+//Function To show notes on the Page
+
 function postNotes() {
+  var pagearea = document.querySelector(".note-box");
   pagearea.innerHTML = "";
   notes.sort(function (a, b) {
     return b.pri - a.pri;
@@ -38,17 +64,18 @@ function postNotes() {
   notes.forEach(e => {
     if (e.state) {
       var note = document.createElement("div");
-      if (e.pri === "1") note.className = `card bg-danger text-white`;
-      else if (e.pri === "2") note.className = `card bg-warning text-white`;
-      else if (e.pri === "3") note.className = `card bg-success text-white  `;
-
+      note.style.transform = `rotate(${randomInteger(-15, 15)}deg)`;
+      if (e.pri === "1") note.className = `card red accent-3 text-white`;
+      else if (e.pri === "2") note.className = `card yellow accent-1`;
+      else if (e.pri === "3") note.className = `card light-green accent-3`;
+      note.className += " hoverable z-depth-1";
       note.innerHTML = `
   <div class="card-body">
   <h2 style="display:none">${e.id}</h2>
   <h3 class="card-title">${e.title}</h3>
   <p class="card-text">${e.content}</p>
   <button class="btn btn-primary float-left edit">Edit</button>
-  <button class="btn btn-danger delete">x</button>
+  <button class="btn btn-danger delete"><i class="material-icons">close</i></button>
   </div>
   `;
       pagearea.appendChild(note);
@@ -61,11 +88,15 @@ function postNotes() {
         if (note.id == element.parentElement.firstElementChild.innerHTML) {
           note.state = 0;
           sendmsg("Note Removed", "warning");
+          deleteNote();
           postNotes();
         }
       });
     });
   });
+
+  //Update The Note
+
   var edits = document.querySelectorAll(".edit");
   edits.forEach(element => {
     element.addEventListener("click", e => {
@@ -81,13 +112,31 @@ function postNotes() {
   });
 }
 
+//Function For Deleting
+
+function deleteNote() {
+  notes = notes.filter(note => {
+    return note.state;
+  });
+  console.log(notes);
+  saveNotes();
+}
+
+//Function for Handling Messages
+
 function sendmsg(msg, msgClass) {
   var message = document.createElement("div");
   message.className = `alert alert-${msgClass} m-2`;
   message.innerHTML = `${msg}`;
-  var coll = document.querySelector(".col-3");
+  var coll = document.querySelector(".addnote");
   coll.insertBefore(message, document.querySelector(".form-container"));
   setTimeout(() => {
     document.querySelector(".alert").remove();
   }, 2000);
+}
+
+//function to generate random number
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
